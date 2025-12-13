52a30d5feb84104fb74a46adfe881cae8470a5cb
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2025-12-13 16:56:35 +0530

    removed ngx-env dependency and used standard angular builder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,10 @@
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
-
 export const environment = {
     production: false,
     github: {
         pdfUri: 'raw.githubusercontent.com/ezazulhaq/library/master/taqwa_tracker',
     },
     api: {
-        map: `https://nominatim.openstreetmap.org/reverse`
-    },
-    supabase: {
-        url: supabaseUrl,
-        anonKey: supabaseAnonKey
+        map: 'https://nominatim.openstreetmap.org/reverse'
     },
     apiBaseUrl: 'https://api.thetaqwatracker.com'
 };
